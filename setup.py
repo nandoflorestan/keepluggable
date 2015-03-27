e769111d127ce353535d4319a847556a09194894
Nando Florestan <nandoflorestan@gmail.com>
2015-03-27 19:38:31 -0300

    Bump version to 0.2.1.dev1 after release

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if version_info[:2] < (3, 4):
 
 setup(
     name='keepluggable',
-    version='0.2.0',
+    version='0.2.1.dev1',
     description='Manages storage of images and other files, with metadata.'
     ' Also offers an HTTP API done on Pyramid.',
     long_description=long_description,
